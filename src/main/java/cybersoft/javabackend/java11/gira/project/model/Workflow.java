7913a26624d3fcfb486d1e580c0a890efdbfad0e
Phat Tai <dptai7@gmail.com>
2021-06-24 21:26:35 +0700

    update

---
+++ src/main/java/cybersoft/javabackend/java11/gira/project/model/Workflow.java
@@ -0,0 +1,19 @@
+package cybersoft.javabackend.java11.gira.project.model;
+
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Workflow {
+	
+	private String name;
+	private String description;
+	private Project project;
+	private Set<WorkflowNode> nodes;
+	private Set<WorkflowLink> links;
+	
+
+}
